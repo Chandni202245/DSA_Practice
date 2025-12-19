128032e3d2fbb33b5c3aa624cfb0f34e2d1d7809
Chandni Kumari <ck623324@gmail.com>
2025-12-19 12:56:25 +0530

    Day 06: Insertion Sort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,16 @@
+def insertion_sort(nums):
+    n=len(nums)
+    for i in range(1,n):
+        key=nums[i]
+        j=i-1
+        while j>=0 and nums[j]>key:
+            nums[j+1]=nums[j]
+            j-=1
+        nums[j+1]=key
+    return nums
+
+nums=[5,8,1,6,9,2,4]
+insertion_sort(nums)
+print(nums)
+
+# TC-> O(n^2)
