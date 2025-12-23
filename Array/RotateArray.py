53c4362e7ab11fe1a8c28011c63492d6a1b63734
Chandni Kumari <ck623324@gmail.com>
2025-12-23 18:56:57 +0530

    Fix array rotation logic

--- Array/RotateArray.py
+++ Array/RotateArray.py
@@ -1,4 +1,16 @@
-def reverse(nums, left, right):
+ # Right Rotate an array by one place
+nums=[5,-2,3,9,0,6,10,7]
+n=len(nums)
+print(nums)
+# nums[:]= [nums[n-1]]+ nums[0:n-1]
+# print("Right Rotate an array by one place: ",nums)
+
+
+# Left Rotate an array
+nums[:]=nums[1:]+nums[:1]
+print("left rotate: ",nums)
+
+"""def reverse(nums, left, right):
     while left < right:
         nums[left], nums[right] = nums[right], nums[left]
         left += 1
@@ -20,4 +32,4 @@ reverse(nums, 0, k - 1)
 # Step 3: reverse remaining elements
 reverse(nums, k, n - 1)
 
-print(nums)
+print(nums)"""
