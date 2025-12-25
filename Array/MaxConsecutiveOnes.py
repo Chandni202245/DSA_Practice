92a4649b17d0216aef8383122387612635b66ef6
Chandni Kumari <ck623324@gmail.com>
2025-12-25 17:11:45 +0530

    Solved max consecutive ones problem in O(n) time and O(1) space

---
+++ Array/MaxConsecutiveOnes.py
@@ -0,0 +1,13 @@
+def maxConsecutiveOnes(nums):
+    count=0
+    max_count=0
+    for i in range(0,len(nums)):
+        if nums[i]==1:
+            count+=1
+        else:
+            max_count=max(max_count,count)
+            count=0
+    return max(max_count,count)
+
+nums=[1,1,0,1,0,1,1,1,1,1,0,1,1]
+print(maxConsecutiveOnes(nums))
