4e556b857be58c8b798d3e41fe1c9480d9ca96b2
Chandni Kumari <ck623324@gmail.com>
2026-01-08 11:41:34 +0530

    Implement linked list reversal and display both original and reversed lists

--- Linked_List/ReverseLinkedList.py
+++ Linked_List/ReverseLinkedList.py
@@ -1,45 +1,39 @@
-from typing import Optional
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+def ReverseLinkedList(head):
+    temp=head
+    prev=None
+    while temp is not None:
+        front=temp.next
+        temp.next=prev
+        prev=temp
+        temp=front
+    return prev
 
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current = head
-        prev = None
+def printLinkedList(head):
+    temp=head
+    while temp is not None:
+        print(temp.data,end="->")
+        temp=temp.next
+    print("None")
 
-        while current:
-            front = current.next
-            current.next = prev
-            prev = current
-            current = front
+head=Node(1)
+head.next=Node(2)
+head.next.next=Node(3)
 
-        return prev
+print("Original List")
+printLinkedList(head)
 
 
-# Creating linked list: 1 -> 2 -> 3 -> 4 -> 5 -> None
-head = ListNode(1)
-head.next = ListNode(2)
-head.next.next = ListNode(3)
-head.next.next.next = ListNode(4)
-head.next.next.next.next = ListNode(5)
+head=ReverseLinkedList(head)
 
-print("Original Linked List:")
-temp = head
-while temp:
-    print(temp.val, end=" -> ")
-    temp = temp.next
-print("None")
+print("Reversed Linked list")
+printLinkedList(head)
+temp=head
 
-# Reverse
-sol = Solution()
-reversed_head = sol.reverseList(head)
 
-print("Reversed Linked List:")
-temp = reversed_head
-while temp:
-    print(temp.val, end=" -> ")
-    temp = temp.next
-print("None")
+# TC->O(N)
+# SC-> O(1)
