fcc020ef837ca5e40415646dc07766e4416c2820
Chandni Kumari <ck623324@gmail.com>
2026-01-08 11:57:36 +0530

    Implement Floyd’s cycle detection algorithm for linked list

---
+++ Linked_List/LinkedList_Cycle.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+def LinkedListCycle(head):
+    slow=head
+    fast=head
+    while fast is not None and fast.next is not None:
+        slow=slow.next
+        fast=fast.next.next
+        if slow==fast:
+            return True
+    return False
+
+# Create nodes
+# head = Node(1)
+# head.next = Node(2)
+# head.next.next = Node(3)
+# head.next.next.next = Node(4)
+
+# # Create cycle: 4 -> 2
+# head.next.next.next.next = head.next
+
+# print(LinkedListCycle(head))  # True
+
+
+# Without cycle
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+
+print(LinkedListCycle(head))  # False
+
